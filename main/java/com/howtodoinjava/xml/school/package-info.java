fbd62ff2e4507428a8dc0dde9ebe0ed6875bca5b
tanmin <tanmin213@gmail.com>
2018-07-02 15:19:44 +0800

    add src dir

--- main/java/com/howtodoinjava/xml/school/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.7 生成的
-// 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// 在重新编译源模式时, 对此文件的所有修改都将丢失。
-// 生成时间: 2018.06.14 时间 12:11:45 AM CST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.howtodoinjava.com/xml/school", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.howtodoinjava.xml.school;
